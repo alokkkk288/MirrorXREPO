e29ff3a7dfd171b94cd0715b7ab58a87ae57211d
srinivas724262 <85380125+alokkkk288@users.noreply.github.com>
2021-07-11 13:11:28 +0530

    MirrorX REPO

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,24 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.MirrorCommand = 'mirrormega1'
+        self.UnzipMirrorCommand = 'unzipmirrormega1'
+        self.TarMirrorCommand = 'tarmirrormega1'
+        self.CancelMirror = 'cancelmega1'
+        self.CancelAllCommand = 'cancelallmega1'
+        self.ListCommand = 'listmega1'
+        self.SpeedCommand = 'speedtestmega1'
+        self.StatusCommand = 'statusmega1'
+        self.AuthorizeCommand = 'authorize'
+        self.UnAuthorizeCommand = 'unauthorize'
+        self.PingCommand = 'ping'
+        self.RestartCommand = 'restartmega1'
+        self.StatsCommand = 'statsmega1'
+        self.HelpCommand = 'help'
+        self.LogCommand = 'log'
+        self.CloneCommand = "fdsfvcdsf"
+        self.WatchCommand = 'watch'
+        self.TarWatchCommand = 'tarwatch'
+        self.deleteCommand = 'del'
+
+BotCommands = _BotCommands()
